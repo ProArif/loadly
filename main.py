7666ff1019d0e04f6452731afad8a7dda39e750c
Arif <34057291+ProArif@users.noreply.github.com>
2025-10-18 04:36:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -54,4 +54,4 @@ def root():
     return {"message": "✅ VidFetch Python backend is running!"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=port)
